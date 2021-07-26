457474f748d241482c4edcf54a9d104f2001aae1
Ömer Fatih YORGANCILAR <39483788+omerfatih97@users.noreply.github.com>
2021-07-26 22:00:52 +0300

    Update weedSpray.py

--- weedSpray.py
+++ weedSpray.py
@@ -1,10 +1,10 @@
 import cv2
 import motor_processes
 
-esik_degeri=110
-def set_deger(deger):
-    global esik_degeri
-    esik_degeri = int(deger)
+limit_val=110
+def set_limit_val(val):
+    global limit_val
+    limit_val = int(val)
 
 
 def control(img):
@@ -13,30 +13,30 @@ def control(img):
             for y in range(120, 240):  # (120,360):
                 r, g, b = img[x, y]
                 F = g - 0.5 * r - 0.5 * b
-                if (F > esik_degeri or F < -10) and (r != 255 and g != 255 and b != 255):
+                if (F > limit_val or F < -10) and (r != 255 and g != 255 and b != 255):
                    img[x,y]=(0, 255, 255)
         cv2.imshow("test",img)
 
 
-def ilacla(img):
-    say = 0
+def insecticide(img):
+    count = 0
     for x in range(2, 120):  # (160,480) :
         for y in range(int(width / 4), int(width / 2)):  # (120,360):
             r, g, b = img[x, y]
             F = g - 0.5 * r - 0.5 * b
-            if (say == 0):
+            if (count == 0):
                 if ((F < -10) or (F > 110)) and (
-                        r > g):#if (F > esik_degeri or F < -10) and (r != 255 and g != 255 and b != 255):
-                    say += 1
-                    print("calisti")
-                    motor_processes.calistir()
+                        r > g):#if (F > limit_val or F < -10) and (r != 255 and g != 255 and b != 255):
+                    count += 1
+                    #print("started")
+                    motor_processes.start_motor()
             else:
-                print("durdu")
-                motor_processes.dur()
-                return say
+                #print("stopped")
+                motor_processes.stop_motor()
+                return count
 
 
-def goruntu_al(secim):
+def taken_view(selection):
     url =0 #"C:/Users/Fatih/Desktop/projevid/test_v.mp4"#('http://192.168.1.103:8080/video')
 
     cam = cv2.VideoCapture(url) #('C:/Users/Fatih/Desktop/vid/tarla.mp4')
@@ -49,23 +49,23 @@ def goruntu_al(secim):
     y = 1
     cv2.rectangle(img, (2 * x, y), (4 * x, y + 120), (0, 255, 0), 2)
 
-    if(secim=="goruntu"):
+    if(selection=="view"):
         control(img)
     else:
-        print(ilacla(img))
+        print(insecticide(img))
 
 
-def islem(yap):
+def choosen_action(proccess):
     while True:
         try:
-            goruntu_al(yap)
+            taken_view(proccess)
 
         except:
             print("Error!!!")
-            motor_processes.dur()
+            motor_processes.stop_motor()
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
          print("Process Stopped!!!")
-         if (yap != "goruntu"):
-            motor_processes.dur()
+         if (proccess != "view"):
+            motor_processes.stop_motor()
          break
